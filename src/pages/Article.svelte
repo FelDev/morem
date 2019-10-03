3de343f5d3346010ed0c6f33e99645ee76ae9816
mesieu <felixparadis@gmail.com>
2019-10-03 18:55:32 -0400

    just testing

---
+++ src/pages/Article.svelte
@@ -0,0 +1,70 @@
+<article class="article">
+	<Intro>
+		<h1 class="title">{ params.title }</h1>
+		<span class="subtitle">A killer story</span>
+		<p class="summary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus laborum eaque sapiente dolorem nisi voluptate explicabo corporis, veritatis vero. Quae voluptates voluptatum ut quis quia alias tenetur impedit quam quaerat.</p>
+
+		<div class="content">
+			<a href="/blog" class="back">Back to Blog</a>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam maiores necessitatibus nihil quo, cupiditate consectetur voluptatem cumque ipsum consequuntur aut repellat repellendus eligendi, placeat inventore perspiciatis dolores ipsa voluptates porro.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam maiores necessitatibus nihil quo, cupiditate consectetur voluptatem cumque ipsum consequuntur aut repellat repellendus eligendi, placeat inventore perspiciatis dolores ipsa voluptates porro.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam maiores necessitatibus nihil quo, cupiditate consectetur voluptatem cumque ipsum consequuntur aut repellat repellendus eligendi, placeat inventore perspiciatis dolores ipsa voluptates porro.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam maiores necessitatibus nihil quo, cupiditate consectetur voluptatem cumque ipsum consequuntur aut repellat repellendus eligendi, placeat inventore perspiciatis dolores ipsa voluptates porro.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam maiores necessitatibus nihil quo, cupiditate consectetur voluptatem cumque ipsum consequuntur aut repellat repellendus eligendi, placeat inventore perspiciatis dolores ipsa voluptates porro.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam maiores necessitatibus nihil quo, cupiditate consectetur voluptatem cumque ipsum consequuntur aut repellat repellendus eligendi, placeat inventore perspiciatis dolores ipsa voluptates porro.</p>
+		</div>
+	</Intro>
+
+</article>
+
+<script>
+	import Intro from '@components/Intro';
+	export let params = {};
+</script>
+
+<style lang="sass">
+	.article
+		margin-top: -64px
+	
+	.title
+		text-transform: capitalize
+	
+	.subtitle
+		opacity: 0.65
+		display: block
+		text-indent: 16px
+		font-style: italic
+		margin-top: -12px
+	
+	.summary
+		padding-top: 16px
+	
+	.content
+		position: relative
+		border-top: 2px solid var(--blue)
+		padding-top: 32px
+		margin-top: 32px
+	
+	.back
+		top: -16px
+		display: block
+		padding: 4px 12px
+		position: absolute
+		margin-bottom: 16px
+		background: var(--blue)
+		border-radius: var(--radius)
+		font-family: var(--font-header)
+		text-transform: uppercase
+		color: var(--offwhite)
+		padding-left: 32px
+		line-height: 20px
+		font-weight: 800
+		font-size: 80%
+		&::after
+			content: '➤'
+			position: absolute
+			transform: rotate(180deg)
+			font-size: 14px
+			left: 10px
+			top: 4px
+</style>
---
+++ src/pages/Blog.svelte
@@ -0,0 +1,62 @@
+<div class="blog">
+	<Intro>
+		<h1>Welcome to my Blog!</h1>
+		<p><span class="callout">NEW</span> articles posted every week. Please be sure to subscribe if you like what you see. 👀 Yadda yadda...</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea nam, ratione natus. Aliquid veritatis illo veniam. Quam tempora quia provident facilis, molestiae iure reiciendis officia, fugit vitae ullam voluptatem quis.</p>
+		<p class="indent">Please select an Article to read:</p>
+	</Intro>
+
+	<div class="blog_grid">
+		{#each items as _item, idx}
+			<a class="blog_item" href={ `/blog/article${idx}` }>
+				<Card>
+					<strong>Article #{idx}</strong>
+					<em>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore enim, natus. Beatae ducimus quasi doloremque ad quam qui dolor, architecto repellendus provident rem nostrum accusamus, magnam voluptate vel voluptas iste.</em>
+				</Card>
+			</a>
+		{/each}
+	</div>
+</div>
+
+<script>
+	import Intro from '@components/Intro';
+	import Card from '@components/Card';
+
+	const items = Array.from({ length:10 });
+</script>
+
+<style lang="sass">
+	.blog
+		margin-top: -64px
+	
+	.indent
+		opacity: 0.85
+		font-style: italic
+		border-left: 4px solid var(--blue)
+		padding: 4px 16px
+	
+	.blog_grid
+		display: grid
+		grid-gap: 16px
+		grid-template-columns: repeat(auto-fit, minmax(300px, 1fr))
+		margin-bottom: 32px
+	
+	.blog_item
+		cursor: pointer
+		&:hover
+			text-decoration: none
+		strong
+			display: block
+			margin-bottom: 8px
+		em
+			font-size: 85%
+			line-height: 1
+	
+	.callout
+		box-shadow: none
+		font-weight: 800
+		text-transform: uppercase
+		font-family: var(--font-header)
+		text-shadow: 2px 2px 0 rgb(255,210,23), 4px 4px 0 rgb(90,199,255)
+		padding-right: 4px
+</style>
