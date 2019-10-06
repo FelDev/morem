69f65494399d3e008333dc0a72bc1dc386ab375d
mesieu <felixparadis@gmail.com>
2019-10-06 19:38:34 -0400

    focus-visible: needs the focus-visible package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import GAnalytics from 'ganalytics';
 import App from '@components/App';
 import './index.scss';
+import focusVisible from 'focus-visible';
 
 window.app = new App({
 	target: document.querySelector('#app')
